f786928c0687bbcd2c77d153b9cc98e52b7b6f7a
Stephen Stebbing <steves@ss.com>
2017-02-26 15:33:23 +1100

    added doxygen docs

--- mmp/mmp_cmd.h
+++ mmp/mmp_cmd.h
@@ -8,7 +8,7 @@
  * @author steves
  * @date   2017/02/21 23:13:41
  * 
- * @brief  
+ * @brief  Command-response messages and async messages on top of MMP protocol.
  * 
  */
 
@@ -29,26 +29,26 @@ typedef  void (*mmp_cmd_handler_t)(void *handle, uint8_t cmd, uint8_t data_len, 
 
 //! Control structure
 typedef struct mmp_cmd_ctrl_t{
-    // mmp control structure
+    //! mmp control structure
     mmp_ctrl_t mmp_ctrl;
-    // function that is called to transmit a byte on the transmission channel
+    //! function that is called to transmit a byte on the transmission channel
     void (*tx_byte_fn)(const char c);
-    // table of message-handler functions. ie pointer to an array of cmd_handler_t
+    //! table of message-handler functions. ie pointer to an array of cmd_handler_t
     mmp_cmd_handler_t *cmd_handler_tab;
-    // number of entries in cmd_handler_tab
+    //! number of entries in cmd_handler_tab
     uint8_t num_handlers;
 }mmp_cmd_ctrl_t;
 
 
 /** 
+ * Initialise the mmp_cmd system.
  * 
- * 
- * @param ctrl 
- * @param msg_buf 
- * @param msg_buf_size 
- * @param cmd_handler_tab 
- * @param num_handlers 
- * @param tx_byte_fn 
+ * @param ctrl Pointer to control-data structure.
+ * @param msg_buf Buffer for received command-messages, and possibly for response messages.
+ * @param msg_buf_size Maximum size of the msg_buf
+ * @param cmd_handler_tab Pointer to table of command-handler callback functions.
+ * @param num_handlers The number of command-handler callback functions in the command-handler table.
+ * @param tx_byte_fn Pointer to the function that will be called to transmit each byte (character) of the response message.
  */
 void mmp_cmd_init(mmp_cmd_ctrl_t *ctrl, uint8_t* msg_buf, uint8_t msg_buf_size, 
 		  mmp_cmd_handler_t *cmd_handler_tab, uint8_t num_handlers,
@@ -56,12 +56,12 @@ void mmp_cmd_init(mmp_cmd_ctrl_t *ctrl, uint8_t* msg_buf, uint8_t msg_buf_size,
 
 
 /** 
+ * Send a command-reply message.
  * 
- * 
- * @param handle 
- * @param status 
- * @param data 
- * @param data_len 
+ * @param handle Pointer to the handle as obtained from call to command-handler function.
+ * @param status The status resulting for command's execution. 0 for success, non-zero otherwise.
+ * @param data Pointer to the data bytes of the response message
+ * @param data_len Length of the response message
  */
 void mmp_cmd_reply(void *handle, uint8_t status, uint8_t *data, uint8_t data_len);
 
@@ -69,18 +69,34 @@ void mmp_cmd_reply(void *handle, uint8_t status, uint8_t *data, uint8_t data_len
 /** 
  * Send a message with flags byte set to indicate that
  * message is asynchronous (ie not a response to a command message)
+ * Used, for example, for MCU button clicks.
  * 
- * @param msg_data 
- * @param len 
+ * @param msg_data Pointer to the message's data.
+ * @param len Lenght of the message's data.
  * @param tx_byte_fn Callback function that is used to send the message's bytes.
  */
 void mmp_async_send(uint8_t *msg_data, uint8_t len, void (*tx_byte_fn)(const char c));
 
+
+/** 
+ * Called to pass a character (byte) that has been received on the communication channel onto the mmp_cmd system.
+ * 
+ * @param msg_cmd_ctrl Pointer to the mmp_cmd_ctrl_t structure.
+ * @param ch The character (byte) that has been received.
+ */
 void mmp_cmd_rx_ch(mmp_cmd_ctrl_t *mmp_cmd_ctrl, uint8_t ch);
 
+/** 
+ * Should be called periodically so message reception timeouts can be detected.
+ * Default implementation calls this at about 1000Hz
+ * @param mmp_cmd_ctrl Pointer to the mmp_cmd_ctrl_t structure.
+ */
 void mmp_cmd_tick(mmp_cmd_ctrl_t *mmp_cmd_ctrl);
 
+#if 0
+// XXX internal function, declaration not required here.
 void mmp_cmd_msg_handler(void *user_data, mmp_msg_t *msg);
+#endif
 
 #endif /* _MMP_CMD_H */
 
--- task.h
+++ task.h
@@ -10,12 +10,14 @@
  * 
  * @brief  Tasking (co-routine) system for AVR.
  * 
+ * 
  */
 
 #include "config.h"
 #include <stdint.h>
 
-// Configuration defines
+// Configuration defines:
+// XXX How to get doxygen to document this? this just don't seem to work.
 //! \def TASK_NUM_TASKS 
 //! \brief The number of tasks in the task table. This must be defined prior to including this file.
 #ifndef TASK_NUM_TASKS 
@@ -33,50 +35,52 @@
 void task_init(uint8_t task_num, void(*task_callback)(void *data), void *data, uint8_t run);
 
 /** 
+ * Make task number task_num unready until the passed number of ticks has occured, ie put task to sleep
+ * for this many ticks.
  * 
- * 
- * @param task_num 
- * @param ticks 
+ * @param task_num The number of task for which the tick timer is to be set.
+ * @param ticks The number of ticks that the task will sleep for.
  */
 void task_num_set_tick_timer(uint8_t task_num, uint16_t ticks);
 
 /** 
- * 
- * 
- * @param ticks 
+ * Put current task to sleep (ie make unrunnable) until the passed number of ticks has occured.
+ * This function would normally only be called from within a task's callback function.
+ * @see task_num_set_tick_timer
+ * @param ticks The numer of ticks that the task will sleep for.
  */
 void task_set_tick_timer(uint16_t ticks);
 
 /** 
- * 
- * 
- * @param task_num 
- * @param ticks 
+ * Put task numbered task_num to sleep for passed number of seconds, ie task will be made unrunnable,
+ * and then runnable again after this number of seconds.
+ * @param task_num The number of the task to be put to sleep.
+ * @param seconds The number of the task for which the second timer is to be set
  */
-void task_num_set_seconds_timer(uint8_t task_num, uint16_t ticks);
+void task_num_set_seconds_timer(uint8_t task_num, uint16_t seconds);
 
 /** 
- * 
- * 
- * @param ticks 
+ * Put current task to sleep for passed number of seconds
+ * @see task_num_set_seconds_timer
+ * @param seconds The number of the task for which the second timer is to be set
  */
-void task_set_seconds_timer(uint16_t ticks);
+void task_set_seconds_timer(uint16_t seconds);
 
 /** 
- * 
- * 
+ * Function that should be called periodically to make the tasks run. This function, in turn, calls the callback function of all
+ * tasks that are in a ready (runnable) state.
  */
 void task_run();
 
 /** 
- * 
- * 
+ * Function that should be called periodically to indicate that a tick period has expired. 
+ * Checks tick timers of all sleeping tasks and make task runnable if timer has expired.
  */
 void task_tick();
 
 /** 
- * 
- * 
+ * Function that should be called every 'second'. Makes tasks whose seconds-timer have expired runnable.
+ * Note that 'second' is defined as a time period that is close to one second.
  */
 void task_seconds_tick();
 
@@ -96,16 +100,16 @@ void task_num_set_callback(uint8_t task_num, void (* callback)(void *data));
 void task_set_callback(void (* callback)(void *data));
 
 /** 
+ * Sets the pointer that is passed to a task's callback function when it is called.
  * 
- * 
- * @param task_num 
- * @param data 
+ * @param task_num The number of the task.
+ * @param data Pointer to the task's user data.
  */
 void task_num_set_user_data(uint8_t task_num, void *data);
 
 /** 
- * 
- * 
+ * Sets the pointer to the task's user data for the current task.
+ * @see task_num_set_user_data
  * @param data Pointer to the user data
  */
 void task_set_user_data(void *data);
