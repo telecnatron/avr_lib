65996eb472886a9ef0699918b21e395831ec9108
Stephen Stebbing <steves@ss.com>
2017-02-26 14:43:54 +1100

    removed rtc and mcui and onewire stuff, this will be reincorporated in a future release.

--- version.h
+++ version.h
@@ -1,10 +1,3 @@
-#ifndef _VERSION_H
-#define _VERSION_H 1
-// -----------------------------------------------------------------------------
-// Copyright Stephen Stebbing 2017. http://telecnatron.com/
-// -----------------------------------------------------------------------------
+#define AVR_LIB_VERSION "1.0"
 
-#define AVR_LIB_VERSION "0.1"
-
-#endif /* _VERSION_H */
 
