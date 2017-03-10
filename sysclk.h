fc3d54af290d3d9d211c1294767b772e1a5240aa
Stephen Stebbing <steves@ss.com>
2017-03-10 15:30:32 +1100

    fixed sysclk to use 1ms tick period

--- sysclk.h
+++ sysclk.h
@@ -26,6 +26,8 @@
 #include "config.h"
 #include <stdint.h>
 
+
+//! Change these to use a different timer:
 //! normal mode, output pin disabled, set CTC mode - clear timer on compare match
 #define SYSCLK_INIT()        T2_OC2A_NONPWM_NORMAL(); T2_WGM_CTC()
 #define SYSCLK_INT_ENABLE()  T2_CMA_E();
@@ -33,13 +35,16 @@
 #define SYSCLK_START()       T2_START();
 #define SYSCLK_STOP()        T2_STOP();
 #define SYSCLK_ISR_NAME      TIMER2_COMPA_vect
-
-//! With FCPU = 16MHz, prescaler 1024 and terminal count 15 gives 
-//! tick (compare match interrupt) period of 1.024ms, ~976Hz
-#define SYSCLK_INIT_PRE()  T2_CS_PRE_1024()
-#define SYSCLK_SET_TC()    T2_OCR2A(15)
+// and change these to set different tick periods.
+//! With FCPU = 16MHz, prescaler 64 and terminal count 250 gives 
+//! tick (compare match interrupt) period of 1ms.
+//! eg tick_period = 64/16M*250 = 0.001
+#define SYSCLK_INIT_PRE()  T2_CS_PRE_64()
+#define SYSCLK_SET_TC()    T2_OCR2A(250)
 //! This many ticks per 'second'
-#define SYSCLK_TICK_FREQ   976
+#define SYSCLK_TICK_FREQ   1000
+
+
 
 //! Initialise and start the clock.
 void sysclk_init();
